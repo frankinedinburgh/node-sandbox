c848005054b2b1fb915c064de6fe3e8ce7eaabfd
frank <>
2018-11-13 23:15:09 +0000

    wip

--- server.js
+++ server.js
@@ -1,47 +1,79 @@
 const dotenv = require('dotenv')
 dotenv.config({path: '.env'});
+const _ = require('lodash')
 const express = require('express')
 const mailer = require('express-mailer');
 const app = express()
 const port = process.env.PORT || 3000
-const tickets = require('./tickets.json')
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
+app.use((req, res, next) => {
+
+    let now = new Date().toString();
+    console.log(now)
+    next();
+})
 
 
 const indexRoute = require('./routes/index')
 const stravaRoute = require('./routes/strava')
+const emailRoute = require('./routes/email')
 const ticketsRoute = require('./routes/tickets')
 
 
 mailer.extend(app, {
-	from: 'frank@altv.com',
-	host: 'smtp.office365.com',
-	secureConnection: false, // use SSL
-	port: 587, // port for secure SMTP
-	transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
-	auth: {
-		user: process.env.EMAIL_ACCOUNT,
-		pass: process.env.EMAIL_PASSWORD
-	}
+    from: 'frank@altv.com',
+    cc: ['frankinedinburgh@yahoo.ie', 'frankhaguemail@gmail.com'],
+    host: 'smtp.office365.com',
+    secureConnection: false, // use SSL
+    port: 587, // port for secure SMTP
+    transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
+    auth: {
+        user: process.env.EMAIL_ACCOUNT,
+        pass: process.env.EMAIL_PASSWORD
+    }
 });
 
 
 app.get('/', function (req, res, next) {
 
-	res.send(tickets)
-	//app.mailer.send('email', {
-	//	to: 'frank@altv.com', // REQUIRED. This can be a comma delimited string just like a normal email to field.
-	//	subject: 'Test Email', // REQUIRED.
-	//	otherProperty: 'Other Property' // All additional properties are also passed to the template as local variables.
-	//}, function (err) {
-	//	if (err) {
-	//		// handle error
-	//		console.log(err);
-	//		res.send('There was an error sending the email');
-	//		return;
-	//	}
-	//	res.send('Email Sent');
-	//});
+	//res.render('email', { name: 'my name is frank'})
+
+    // app.mailer.send('email', {  // send the email
+    app.mailer.render('email', {
+        to: ['frank@altv.com', 'frankinedinburgh@yahoo.ie'],
+        subject: 'Test Email'
+    }, function (err, message) {
+        if (err) {
+            // handle error
+            console.log(err);
+            res.send('There was an error rendering the email');
+            return;
+        }
+        res.header('Content-Type', 'text/plain');
+        res.send(message);
+    }, { name: 'my name is frank'});
+
+});
+
+
+app.get('/send', function (req, res, next) {
+
+    //res.render('email', { name: 'my name is frank'})
+
+    app.mailer.send('email', {  // send the email
+        to: ['frank@altv.com', 'frankinedinburgh@yahoo.ie'],
+        subject: 'Test Email'
+    }, function (err, message) {
+        if (err) {
+            // handle error
+            console.log(err);
+            res.send('There was an error rendering the email');
+            return;
+        }
+        res.header('Content-Type', 'text/plain');
+        res.send(message);
+    }, { name: 'my name is frank'});
+
 });
 
 
@@ -57,6 +89,7 @@ app.get('/', function (req, res, next) {
 app.get('/', indexRoute);
 app.get('/strava/', stravaRoute);
 app.get('/tickets/', ticketsRoute);
+app.get('/email/', emailRoute);
 
 
 
